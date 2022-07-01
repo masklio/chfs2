1b6ce2a2db5d04734a61ad3d81bbf3978d834556
masklio <105109469+masklio@users.noreply.github.com>
2022-07-01 10:42:38 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>ONEPIECES</title>
+        <title>Chinese historical figures</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
